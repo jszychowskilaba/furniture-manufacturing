3395f19a9d17cd046c12eabf5f2ebcf6c0e2534f
Jose Szychowski <jszychowski.laba@solvd.com>
2023-10-09 19:16:49 -0300

    update function names and add description

--- src/services/authServices.ts
+++ src/services/authServices.ts
@@ -21,7 +21,7 @@ const isValidCredentials = (userCredentials: UserCredentials): boolean => {
 };
 
 /**
- * Updates token and refreshToken with expiration time, refresh token 
+ * Updates token and refreshToken with expiration time, refresh token
  * and return them. It works by:
  * 1. Storing tokens with user information as key, and token as value.
  *  These pair of key values are used for tracking the tokens for
@@ -38,18 +38,22 @@ const updateTokens = async (userCredentials: UserCredentials): Promise<string[]>
   // Creating access key
   const tokenKey = `${userCredentials.username}.token`;
   const refreshTokenKey = `${userCredentials.username}.refreshToken`;
-  // Getting old tokens
-  const oldToken = await Auth.getTokenAsync(tokenKey);
-  const oldRefreshToken = await Auth.getTokenAsync(refreshTokenKey);
 
-  // Deleting old tokens
+  // Getting old tokens
+  const oldToken = await Auth.getAsync(tokenKey);
+  const oldRefreshToken = await Auth.getAsync(refreshTokenKey);
+
   if (oldToken) {
-    await Auth.deleteTokenAsync(oldToken);
-    await Auth.deleteTokenAsync(tokenKey);
+    // Deleting old token access key
+    await Auth.deleteAsync(oldToken);
+    // Deleting old token
+    await Auth.deleteAsync(tokenKey);
   }
   if (oldRefreshToken) {
-    Auth.deleteTokenAsync(oldRefreshToken);
-    Auth.deleteTokenAsync(refreshTokenKey);
+    // Deleting old refresh token
+    Auth.deleteAsync(oldRefreshToken);
+    // Deleting old refresh token access key
+    Auth.deleteAsync(refreshTokenKey);
   }
 
   // Creating new tokens
@@ -59,14 +63,14 @@ const updateTokens = async (userCredentials: UserCredentials): Promise<string[]>
   const newRefreshTokenExpiration = Number(process.env.REFRESH_TOKEN_EXPIRATION) || 19200;
 
   // Storing new tokens
-  await Auth.storeTokenAsync(tokenKey, newToken, newTokenExpiration);
-  await Auth.storeTokenAsync(newToken, tokenKey, newTokenExpiration);
-  await Auth.storeTokenAsync(
+  await Auth.storeAsync(tokenKey, newToken, newTokenExpiration);
+  await Auth.storeAsync(newToken, tokenKey, newTokenExpiration);
+  await Auth.storeAsync(
     refreshTokenKey,
     newRefreshToken,
     newRefreshTokenExpiration,
   );
-  await Auth.storeTokenAsync(
+  await Auth.storeAsync(
     newRefreshToken,
     refreshTokenKey,
     newRefreshTokenExpiration,
