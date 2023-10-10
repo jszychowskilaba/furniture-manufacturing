08cfc7f5878e3742e1df618ae9140fb45b6b161c
Jose Szychowski <jszychowski.laba@solvd.com>
2023-10-09 21:06:47 -0300

    modify comments, remove credential validation and add refreshToken function

--- src/services/authServices.ts
+++ src/services/authServices.ts
@@ -1,7 +1,7 @@
 /* eslint-disable no-useless-catch */
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { v4 as uuid } from 'uuid';
-import { UserCredentials } from '../types';
+import { UserCredentials, Error } from '../types';
 import * as Credentials from '../databases/Credentials';
 import * as Auth from '../databases/Auth';
 
@@ -21,8 +21,8 @@ const isValidCredentials = (userCredentials: UserCredentials): boolean => {
 };
 
 /**
- * Updates token and refreshToken with expiration time, refresh token
- * and return them. It works by:
+ * Updates token and refreshToken with expiration time stored
+ * in .env, and return them. It works by:
  * 1. Storing tokens with user information as key, and token as value.
  *  These pair of key values are used for tracking the tokens for
  *  updating them.
@@ -30,11 +30,9 @@ const isValidCredentials = (userCredentials: UserCredentials): boolean => {
  *  These pair of tokens are the used for OAuth, so no user information
  *  is needed to validate the user.
  * @param userCredentials The credential
- * @returns The tokens
+ * @returns The new tokens
  */
 const updateTokens = async (userCredentials: UserCredentials): Promise<string[]> => {
-  isValidCredentials(userCredentials);
-
   // Creating access key
   const tokenKey = `${userCredentials.username}.token`;
   const refreshTokenKey = `${userCredentials.username}.refreshToken`;
@@ -79,5 +77,32 @@ const updateTokens = async (userCredentials: UserCredentials): Promise<string[]>
   return [newToken, newRefreshToken];
 };
 
+/**
+ * Refresh the token and refresh token if the incoming
+ * refresh token is valid
+ * @param refreshToken The refresh token
+ * @returns The new tokens
+ */
+const refreshTokens = async (refreshToken: string): Promise<string[]> => {
+  // Getting username using the refresh token
+  const userNameKey = await Auth.getAsync(refreshToken); // <username>.refreshToken
+  if (!userNameKey) {
+    const error: Error = {
+      status: 401,
+      message: 'Invalid refresh token',
+    };
+    // eslint-disable-next-line @typescript-eslint/no-throw-literal
+    throw error;
+  }
+  // Preparing username in correct forman
+  const userName = userNameKey.slice(0, -13); // username (removes <.refreshToken>)
+  // Preparing user credentials to be able to update the tokens
+  const userCredentials: UserCredentials = {
+    username: userName,
+    password: null, // setting null value to have the corresponding type
+  };
+  return updateTokens(userCredentials);
+};
+
 // eslint-disable-next-line import/prefer-default-export
-export { updateTokens };
+export { updateTokens, refreshTokens, isValidCredentials };
