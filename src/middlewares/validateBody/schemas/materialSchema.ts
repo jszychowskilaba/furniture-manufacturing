b6bae4ee46d716c1b76d9505498cb5db6b15be0c
Jose Szychowski <jszychowski.laba@solvd.com>
2023-10-14 17:07:11 -0300

    create schema

---
+++ src/middlewares/validateBody/schemas/materialSchema.ts
@@ -0,0 +1,25 @@
+import { JSONSchemaType } from 'ajv';
+import { Material } from '../../../types/types';
+
+export const materialSchema: JSONSchemaType<Material> = {
+  type: 'object',
+  properties: {
+    internalCode: { type: 'string', minLength: 0, maxLength: 255 },
+    description: { type: 'string', minLength: 1, maxLength: 255 },
+    quantity: { type: 'number', minimum: 0 },
+    pricePerUnit: { type: 'number', minimum: 0 },
+    unit: { type: 'string', minLength: 1, maxLength: 255 },
+    purchaseTime: { type: 'number', minimum: 0 },
+    internalNotes: { type: 'string', minLength: 1, maxLength: 255 },
+  },
+  required: [
+    'internalCode',
+    'description',
+    'quantity',
+    'pricePerUnit',
+    'unit',
+    'purchaseTime',
+    'internalNotes',
+  ],
+  additionalProperties: false,
+};
