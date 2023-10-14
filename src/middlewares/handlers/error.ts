3db8b17b6988522e903297d5962fe2ac70e5e961
Jose Szychowski <jszychowski.laba@solvd.com>
2023-10-14 18:55:11 -0300

    create error handler

---
+++ src/middlewares/handlers/error.ts
@@ -0,0 +1,11 @@
+import { Error } from '../../types/types';
+import { Request, Response, NextFunction } from 'express';
+
+const errorHandler = (
+  error: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  res.status(error.status).json(error.message);
+};
