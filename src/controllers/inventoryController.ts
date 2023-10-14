623ac79b639ec97022a1cede965e7771b237f004
Jose Szychowski <jszychowski.laba@solvd.com>
2023-10-14 17:10:07 -0300

    create controler

---
+++ src/controllers/inventoryController.ts
@@ -0,0 +1,6 @@
+import { Request, Response } from 'express';
+import * as inventoryServices from '../services/inventoryServices';
+
+const createMaterial = async (req: Request, res: Response): Promise<void> => {
+   
+};
