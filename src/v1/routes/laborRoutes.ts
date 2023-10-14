722e418bf5d7a1409bcfd117eb8f0c0d57d920b2
Jose Szychowski <jszychowski.laba@solvd.com>
2023-10-14 20:06:28 -0300

    add validation middleware

--- src/v1/routes/laborRoutes.ts
+++ src/v1/routes/laborRoutes.ts
@@ -1,11 +1,12 @@
 import express, { Router } from 'express';
 import * as laborController from '../../controllers/laborController';
+import { validateBody, laborSchema } from '../../middlewares/validateBody';
 
 const router: Router = express.Router();
 
 router.get('/', laborController.getAllLabors);
-router.post('/', laborController.createLabor);
+router.post('/', validateBody(laborSchema), laborController.createLabor);
 router.get('/:id', laborController.getOneLabor);
-router.put('/:id', laborController.updateLabor);
+router.put('/:id', validateBody(laborSchema), laborController.updateLabor);
 
 export default router;
