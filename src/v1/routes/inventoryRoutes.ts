90d59e9cc6a21f4e8dfd5b029c0353242fa98c40
Jose Szychowski <jszychowski.laba@solvd.com>
2023-10-14 18:13:41 -0300

    add body validation

--- src/v1/routes/inventoryRoutes.ts
+++ src/v1/routes/inventoryRoutes.ts
@@ -1,11 +1,20 @@
 import express, { Router } from 'express';
 import * as inventoryController from '../../controllers/inventoryController';
+import { validateBody, materialSchema } from '../../middlewares/validateBody';
 
 const router: Router = express.Router();
 
 router.get('/', inventoryController.getAllMaterials);
-router.post('/', inventoryController.createMaterial);
+router.post(
+  '/',
+  validateBody(materialSchema),
+  inventoryController.createMaterial
+);
 router.get('/:id', inventoryController.getOneMaterial);
-router.put('/:id', inventoryController.updateMaterial);
+router.put(
+  '/:id',
+  validateBody(materialSchema),
+  inventoryController.updateMaterial
+);
 
 export default router;
