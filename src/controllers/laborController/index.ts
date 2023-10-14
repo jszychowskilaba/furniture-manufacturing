b5768ab6a21273847f359ec9fe50641cef9fc899
Jose Szychowski <jszychowski.laba@solvd.com>
2023-10-14 19:48:42 -0300

    create controller export

---
+++ src/controllers/laborController/index.ts
@@ -0,0 +1,6 @@
+import { getAllLabors } from './getAllLabors';
+import { createLabor } from './createLabor';
+import { getOneLabor } from './getOneLabor';
+import { updateLabor } from './updateLabor';
+
+export { getAllLabors, createLabor, getOneLabor, updateLabor };
