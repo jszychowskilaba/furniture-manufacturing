9971e46f7132a6d11fb5128299ee61ebb259b8bf
Jose Szychowski <jszychowski.laba@solvd.com>
2023-10-15 09:32:42 -0300

    create partial schema for patch method

--- src/middlewares/validateBody/schemas/laborSchema.ts
+++ src/middlewares/validateBody/schemas/laborSchema.ts
@@ -1,10 +1,10 @@
 import { JSONSchemaType } from 'ajv';
-import { Labor } from '../../../types/types';
+import { Labor, PartialLabor } from '../../../types/types';
 
 export const laborSchema: JSONSchemaType<Labor> = {
   type: 'object',
   properties: {
-    status: { type: 'string', enum: ['active', 'inactive'], nullable: true },
+    status: { type: 'string', enum: ['active', 'inactive'] },
     internalCode: { type: 'string', minLength: 0, maxLength: 255 },
     description: { type: 'string', minLength: 1, maxLength: 255 },
     pricePerUnit: { type: 'number', minimum: 0 },
@@ -20,5 +20,34 @@ export const laborSchema: JSONSchemaType<Labor> = {
     'unit',
     'internalNotes',
   ],
+  additionalProperties: false,
+};
+
+export const partialLaborSchema: JSONSchemaType<PartialLabor> = {
+  type: 'object',
+  properties: {
+    status: { type: 'string', enum: ['active', 'inactive'], nullable: true },
+    internalCode: {
+      type: 'string',
+      minLength: 0,
+      maxLength: 255,
+      nullable: true,
+    },
+    description: {
+      type: 'string',
+      minLength: 1,
+      maxLength: 255,
+      nullable: true,
+    },
+    pricePerUnit: { type: 'number', minimum: 0, nullable: true },
+    timePerUnit: { type: 'number', minimum: 0, nullable: true },
+    unit: { type: 'string', minLength: 1, maxLength: 255, nullable: true },
+    internalNotes: {
+      type: 'string',
+      minLength: 1,
+      maxLength: 255,
+      nullable: true,
+    },
+  },
   additionalProperties: false,
 };
--- src/middlewares/validateBody/schemas/materialSchema.ts
+++ src/middlewares/validateBody/schemas/materialSchema.ts
@@ -1,10 +1,10 @@
 import { JSONSchemaType } from 'ajv';
-import { Material } from '../../../types/types';
+import { Material, PartialMaterial } from '../../../types/types';
 
 export const materialSchema: JSONSchemaType<Material> = {
   type: 'object',
   properties: {
-    status: { type: 'string', enum: ['active', 'inactive'], nullable: true },
+    status: { type: 'string', enum: ['active', 'inactive'] },
     internalCode: { type: 'string', minLength: 0, maxLength: 255 },
     description: { type: 'string', minLength: 1, maxLength: 255 },
     quantity: { type: 'number', minimum: 0 },
@@ -22,5 +22,20 @@ export const materialSchema: JSONSchemaType<Material> = {
     'purchaseTime',
     'internalNotes',
   ],
+  additionalProperties: false,
+};
+
+export const partialMaterialSchema: JSONSchemaType<PartialMaterial> = {
+  type: 'object',
+  properties: {
+    status: { type: 'string', enum: ['active', 'inactive'], nullable: true },
+    internalCode: { type: 'string', minLength: 0, maxLength: 255, nullable: true },
+    description: { type: 'string', minLength: 1, maxLength: 255, nullable: true },
+    quantity: { type: 'number', minimum: 0, nullable: true },
+    pricePerUnit: { type: 'number', minimum: 0, nullable: true },
+    unit: { type: 'string', minLength: 1, maxLength: 255, nullable: true },
+    purchaseTime: { type: 'number', minimum: 0, nullable: true },
+    internalNotes: { type: 'string', minLength: 1, maxLength: 255, nullable: true },
+  },
   additionalProperties: false,
 };
