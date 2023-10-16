6efa42c2da9a6c7ddacd4e8f23b01e845df0f6ec
Jose Szychowski <jszychowski.laba@solvd.com>
2023-10-16 18:25:11 -0300

    Add body validation to post

--- src/v1/routes/orderRoutes.ts
+++ src/v1/routes/orderRoutes.ts
@@ -1,10 +1,11 @@
 import express, { Router } from 'express';
 import * as orderController from '../../controllers/orderController';
+import { validateBody, orderSchema } from '../../middlewares/validateBody';
 
 const router: Router = express.Router();
 
 router.get('/', orderController.getAllOrders);
-router.post('/', orderController.createOrder);
+router.post('/', validateBody(orderSchema), orderController.createOrder);
 router.get('/:id', orderController.getOneOrder);
 router.patch('/:id', orderController.updateOrder);
 router.delete('/:id', orderController.deleteOrder);
