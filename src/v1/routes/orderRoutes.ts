f8a42d5f5f7b7628999deb19538b4955a3a83006
Jose Szychowski <jszychowski.laba@solvd.com>
2023-10-16 18:46:02 -0300

    Add body validation

--- src/v1/routes/orderRoutes.ts
+++ src/v1/routes/orderRoutes.ts
@@ -1,13 +1,13 @@
 import express, { Router } from 'express';
 import * as orderController from '../../controllers/orderController';
-import { validateBody, orderSchema } from '../../middlewares/validateBody';
+import { validateBody, orderSchema, partialOrderSchema } from '../../middlewares/validateBody';
 
 const router: Router = express.Router();
 
 router.get('/', orderController.getAllOrders);
 router.post('/', validateBody(orderSchema), orderController.createOrder);
 router.get('/:id', orderController.getOneOrder);
-router.patch('/:id', orderController.updateOrder);
+router.patch('/:id', validateBody(partialOrderSchema), orderController.updateOrder);
 router.delete('/:id', orderController.deleteOrder);
 
 export default router;
