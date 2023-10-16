a2c06fdc0ce4305e72a647ac8140582af642fab6
Jose Szychowski <jszychowski.laba@solvd.com>
2023-10-16 17:06:43 -0300

    Create export file

---
+++ src/controllers/orderController/index.ts
@@ -0,0 +1,7 @@
+import { createOrder } from './createOrder';
+import { deleteOrder } from './deleteOrder';
+import { getAllOrders } from './getAllOrders';
+import { getOneOrder } from './getOneOrder';
+import { updateOrder } from './updateOrder';
+
+export { createOrder, deleteOrder, getAllOrders, getOneOrder, updateOrder };
