78ddb9c60eba10fe1be233ee26286724d2ad6535
Jose Szychowski <jszychowski.laba@solvd.com>
2023-10-22 17:06:06 -0300

    Add documentation

--- src/utils/throwError.ts
+++ src/utils/throwError.ts
@@ -1,5 +1,11 @@
 import { Error } from '../types/types';
 
+/**
+ * Throws a error with the correct shape in order to 
+ * be handled by the corresponding error handler.
+ * @param status The status code
+ * @param message The error message
+ */
 export const throwError = (status: number, message: string) => {
   const error: Error = {
     status: status || 500,
