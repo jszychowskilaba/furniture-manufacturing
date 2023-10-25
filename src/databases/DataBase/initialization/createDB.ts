d40f3f164781aea75dd4bc233eb011975acca06c
Jose Szychowski <jszychowski.laba@solvd.com>
2023-10-25 00:05:36 -0300

    Create function

---
+++ src/databases/DataBase/initialization/createDB.ts
@@ -0,0 +1,31 @@
+import { Client } from 'pg';
+import { config } from '../config';
+
+/**
+ * Creates database if it does not exists.
+ */
+export const createDB = async (): Promise<void> => {
+  const client = new Client({
+    host: config.host,
+    user: config.user,
+    password: config.password,
+    port: config.port,
+  });
+
+  try {
+    await client.connect();
+
+    const res = await client.query(
+      `SELECT datname FROM pg_catalog.pg_database WHERE datname = '${config.database}'`
+    );
+
+    if (!res.rowCount) {
+      await client.query(`CREATE DATABASE "${config.database}";`);
+      console.log(`Database created: ${config.database}.`);
+    } else {
+      console.log(`Not necessary to create database: ${config.database}.`);
+    }
+  } finally {
+    await client.end();
+  }
+};
