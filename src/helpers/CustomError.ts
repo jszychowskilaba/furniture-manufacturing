5701fae61284fdc38eadaba96bbba1ef26677b4d
Jose Szychowski <jszychowski.laba@solvd.com>
2023-10-27 13:09:09 -0300

    Change file location

---
+++ src/helpers/CustomError.ts
@@ -0,0 +1,9 @@
+import { ICustomError } from '../types/types';
+
+export class CustomError extends Error implements ICustomError {
+  status: number;
+  constructor(message: string, status: number) {
+    super(message || 'Unexpected error');
+    this.status = status || 500;
+  }
+}
