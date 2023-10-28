5a6f24488f31b9507acb53ea056594999426b948
Jose Szychowski <jszychowski.laba@solvd.com>
2023-10-28 18:20:53 -0300

    Refactor controller into class

--- src/controllers/laborController/createLabor.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-
-export const createLabor = (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  res.status(200).json('I want to create a labor');
-};
--- src/controllers/laborController/getAllLabors.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-
-export const getAllLabors = (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  res.status(200).json('I want to get all labors');
-};
