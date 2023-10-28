c7e93a5c5d0c1f0469ed679476bb2898b7c8cedb
Jose Szychowski <jszychowski.laba@solvd.com>
2023-10-28 09:09:16 -0300

    Refactor: implement class instead of functions

--- src/controllers/inventoryController/index.ts
+++ src/controllers/inventoryController/index.ts
@@ -1,6 +1,37 @@
-import { createMaterial } from './createMaterial';
-import { getAllMaterials } from './getAllMaterials';
-import { getOneMaterial } from './getOneMaterial';
-import { updateMaterial } from './updateMaterial';
+import inventoryServices from '../../services/inventoryServices';
+import { Request, Response, NextFunction } from 'express';
+import { Material } from '../../types/Material';
 
-export { createMaterial, getAllMaterials, getOneMaterial, updateMaterial };
+class InventoryController {
+  async createMaterial(req: Request, res: Response, next: NextFunction) {
+    try {
+      const createdMaterial = await inventoryServices.createMaterial(
+        req.body as Material,
+        req.headers.username as string
+      );
+      res.status(201).json(createdMaterial);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  getAllMaterials = (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).json('I want to get all materials');
+  };
+
+  getOneMaterial = (req: Request, res: Response, next: NextFunction) => {
+    const id = req.params.id;
+    res
+      .status(200)
+      .json(`I want to get one material. The material ID is: ${id}`);
+  };
+
+  updateMaterial = (req: Request, res: Response, next: NextFunction) => {
+    const id = req.params.id;
+    res
+      .status(200)
+      .json(`I want to update a material. The material ID is: ${id}`);
+  };
+}
+
+export default new InventoryController();
