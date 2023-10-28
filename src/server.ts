c8ac5237153325377268e0f099114b84f2a365f1
Jose Szychowski <jszychowski.laba@solvd.com>
2023-10-28 09:04:58 -0300

    Add database initialization

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,16 @@
 import app from './index';
 import { TokensDB } from './databases/Auth';
+import { initializeDataBase } from './databases/DataBase/initialization';
 
 // Starting redis client for Auth Data Base
 // AuthDB docker container must be already running
 (async () => {
+  try {
+    await initializeDataBase();
+  } catch (error) {
+    console.log(`Data Base error. ${error}`);
+  }
+
   TokensDB.on('error', (error: unknown) => {
     throw error;
   });
@@ -12,11 +19,11 @@ import { TokensDB } from './databases/Auth';
   } catch (error) {
     console.log('Redis TokensDB', error);
   }
+
+  const PORT: number = Number(process.env.PORT) || 3000;
+
+  // Staring server
+  app.listen(PORT, () => {
+    console.log(`API listening port ${PORT}...`);
+  });
 })();
-
-const PORT: number = Number(process.env.PORT) || 3000;
-
-// Staring server
-app.listen(PORT, () => {
-  console.log(`API listening port ${PORT}...`);
-});
