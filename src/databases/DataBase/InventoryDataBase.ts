d05f1fe4adfa41e89ec01a8d39b9391a80f946f3
Jose Szychowski <jszychowski.laba@solvd.com>
2023-10-28 10:46:05 -0300

    Create getOneMaterial method and add comments

--- src/databases/DataBase/InventoryDataBase.ts
+++ src/databases/DataBase/InventoryDataBase.ts
@@ -18,6 +18,12 @@ class InventoryDataBase {
     await pool.query(query);
   }
 
+  /**
+   * Check if a material exist given a column and a value
+   * @param column  The column
+   * @param value The value
+   * @returns <true> if material exists | <false> if material does not exists
+   */
   async hasMaterialWith(column: string, value: string): Promise<boolean> {
     const result = await pool.query(
       selectByTableColumnValueQuery(this.tableName, column, value)
@@ -32,6 +38,20 @@ class InventoryDataBase {
   async getAllMaterials(): Promise<CreatedMaterial[]> {
     const query = selectByRowQuery(this.tableName, '*');
     return (await pool.query(query)).rows;
+  }
+
+  /**
+   * Get one material from database
+   * @param materialId The material id
+   * @returns The material
+   */
+  async getOneMaterial(materialId: string): Promise<CreatedMaterial> {
+    const query = selectByTableColumnValueQuery(
+      this.tableName,
+      'id',
+      materialId
+    );
+    return (await pool.query(query)).rows[0];
   }
 }
 
