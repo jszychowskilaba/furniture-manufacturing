5565e6053840df4459100f903d3d856d1d7b2213
Jose Szychowski <jszychowski.laba@solvd.com>
2023-10-29 10:46:33 -0300

    Refactor class

--- src/databases/DataBase/InventoryDataBase.ts
+++ src/databases/DataBase/InventoryDataBase.ts
@@ -1,76 +1,33 @@
 import { CreatedMaterial, PartialCreatedMaterial } from '../../types/Material';
-import { selectByTableColumnValueQuery } from './helpers/selectByQuery';
-import { selectByRowQuery } from './helpers/selectByRowQuery';
-import { insertQuery } from './helpers/insertQuery';
-import { updateQuery } from './helpers/updateQuery';
-import { pool } from './Pool';
+import { CRUDOperations } from './helpers/CRUDOperations';
 
 class InventoryDataBase {
-  private tableName: string;
+  private operations: CRUDOperations<CreatedMaterial>;
   constructor() {
-    this.tableName = 'material';
+    this.operations = new CRUDOperations('material', 'id');
   }
-  /**
-   * Stores a created material in the database
-   * @param material The material
-   */
+
   async createMaterial(material: CreatedMaterial): Promise<void> {
-    const query = insertQuery(this.tableName, material);
-    await pool.query(query);
+    await this.operations.create(material);
   }
 
-  /**
-   * Check if a material exist given a column and a value
-   * @param column  The column
-   * @param value The value
-   * @returns <true> if material exists | <false> if material does not exists
-   */
   async hasMaterialWith(column: string, value: string): Promise<boolean> {
-    const result = await pool.query(
-      selectByTableColumnValueQuery(this.tableName, column, value)
-    );
-    return !(result.rows[0] === undefined);
+    return await this.operations.hasDataWith(column, value);
   }
 
-  /**
-   * Get all materials from database
-   * @returns The materials
-   */
   async getAllMaterials(): Promise<CreatedMaterial[]> {
-    const query = selectByRowQuery(this.tableName, '*');
-    return (await pool.query(query)).rows;
+    return await this.operations.getAllData();
   }
 
-  /**
-   * Get one material from database
-   * @param materialId The material id
-   * @returns The material
-   */
   async getOneMaterial(materialId: string): Promise<CreatedMaterial> {
-    const query = selectByTableColumnValueQuery(
-      this.tableName,
-      'id',
-      materialId
-    );
-    return (await pool.query(query)).rows[0];
+    return await this.operations.getOneData(materialId);
   }
 
-  /**
-   * Update a material in the database
-   * @param materialId The material to update
-   * @param materialUpdates The object that contains the updates
-   */
   async updateMaterial(
     materialId: string,
     materialUpdates: PartialCreatedMaterial
   ): Promise<void> {
-    const query = updateQuery(
-      this.tableName,
-      materialUpdates,
-      'id',
-      materialId
-    );
-    await pool.query(query);
+    await this.operations.updateData(materialId, materialUpdates);
   }
 }
 
