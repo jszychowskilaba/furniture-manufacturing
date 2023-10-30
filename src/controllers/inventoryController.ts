7977b36a58c24f8122a9f63ea65af1816f0bf75f
Jose Szychowski <jszychowski.laba@solvd.com>
2023-10-30 15:44:29 -0300

    Change file location

---
+++ src/controllers/inventoryController.ts
@@ -0,0 +1,28 @@
+import inventoryServices from '../services/inventoryServices';
+import { Request, Response, NextFunction } from 'express';
+import { ICRUDController } from '../types/ICRUDController';
+import { CRUDController } from './helpers/CRUDController';
+
+class InventoryController {
+  private CRUDController: ICRUDController;
+  constructor() {
+    this.CRUDController = new CRUDController(inventoryServices);
+  }
+  createMaterial = async (req: Request, res: Response, next: NextFunction) => {
+    await this.CRUDController.create(req, res, next);
+  };
+
+  getAllMaterials = async (req: Request, res: Response, next: NextFunction) => {
+    await this.CRUDController.getAll(req, res, next);
+  };
+
+  getOneMaterial = async (req: Request, res: Response, next: NextFunction) => {
+    await this.CRUDController.getOne(req, res, next);
+  };
+
+  updateMaterial = async (req: Request, res: Response, next: NextFunction) => {
+    await this.CRUDController.update(req, res, next);
+  };
+}
+
+export default new InventoryController();
