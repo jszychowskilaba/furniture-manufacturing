b6cc7cc4e12b2fc925e45e798c7c409076f4dd7b
Jose Szychowski <jszychowski.laba@solvd.com>
2023-10-30 20:53:44 -0300

    New feature: User controller

---
+++ src/databases/DataBase/UserDataBase.ts
@@ -0,0 +1,32 @@
+import { IDataBase } from '../../types/IDataBase';
+import { CreatedUser, PartialCreatedUser } from '../../types/User';
+import { CRUDODataBase } from './helpers/CRUDODataBase';
+
+class UserDataBase implements IDataBase<CreatedUser, PartialCreatedUser> {
+  private operations: CRUDODataBase<CreatedUser>;
+  constructor() {
+    this.operations = new CRUDODataBase('appUser', 'username');
+  }
+
+  async create(user: CreatedUser): Promise<void> {
+    await this.operations.create(user);
+  }
+
+  async hasWith(column: string, value: string): Promise<boolean> {
+    return await this.operations.hasWith(column, value);
+  }
+
+  async getAll(): Promise<CreatedUser[]> {
+    return await this.operations.getAll();
+  }
+
+  async getOne(userId: string): Promise<CreatedUser> {
+    return await this.operations.getOne(userId);
+  }
+
+  async update(userId: string, userUpdates: PartialCreatedUser): Promise<void> {
+    await this.operations.update(userId, userUpdates);
+  }
+}
+
+export default new UserDataBase();
