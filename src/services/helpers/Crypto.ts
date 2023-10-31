cb3186ecad370a51c60d9ebd1e285792084e7d7d
Jose Szychowski <jszychowski.laba@solvd.com>
2023-10-30 21:47:09 -0300

    Add createPassword and hashedPassword methods

--- src/services/helpers/Crypto.ts
+++ src/services/helpers/Crypto.ts
@@ -1,1 +1,45 @@
-import crypto from 'crypto';
+import * as crypto from 'crypto';
+
+interface ICreatedPassword {
+  hashedPassword: string;
+  salt: string;
+}
+
+interface ICrypto {
+  createPassword(userPassword: string): ICreatedPassword;
+  isValidPassword(
+    userPassword: string,
+    storedHash: string,
+    storedSalt: string
+  ): boolean;
+}
+
+export class Crypto implements ICrypto {
+  createPassword(userPassword: string): ICreatedPassword {
+    const salt = crypto.randomBytes(16).toString('hex') as string;
+
+    const hashedPassword = crypto
+      .createHmac('sha256', salt)
+      .update(userPassword)
+      .digest('hex') as string;
+
+    const createdPassword: ICreatedPassword = {
+      hashedPassword: hashedPassword,
+      salt: salt,
+    };
+
+    return createdPassword;
+  }
+
+  isValidPassword(
+    userPassword: string,
+    storedHash: string,
+    storedSalt: string
+  ): boolean {
+    const hashedPassword = crypto
+      .createHmac('sha256', storedSalt)
+      .update(userPassword)
+      .digest('hex');
+    return hashedPassword === storedHash;
+  }
+}
