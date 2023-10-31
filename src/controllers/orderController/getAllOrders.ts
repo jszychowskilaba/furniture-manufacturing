90fc0ff6ea709eee095cec699db29763b0b1d1e9
Jose Szychowski <jszychowski.laba@solvd.com>
2023-10-31 11:09:49 -0300

    Refactor to use class

--- src/controllers/orderController/getAllOrders.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-
-export const getAllOrders = (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  res.status(200).json('I want to get all orders');
-};
--- src/controllers/orderController/index.ts
+++
@@ -1,7 +0,0 @@
-import { createOrder } from './createOrder';
-import { deleteOrder } from './deleteOrder';
-import { getAllOrders } from './getAllOrders';
-import { getOneOrder } from './getOneOrder';
-import { updateOrder } from './updateOrder';
-
-export { createOrder, deleteOrder, getAllOrders, getOneOrder, updateOrder };
