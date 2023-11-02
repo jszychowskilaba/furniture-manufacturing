f54f8964c0385a05aa4e3ebd5538719823d4dcc6
Jose Szychowski <jszychowski.laba@solvd.com>
2023-11-02 14:52:01 -0300

    Update env variable names

--- src/config/DataBase.ts
+++ src/config/DataBase.ts
@@ -1,10 +1,11 @@
 const config = {
-  user: process.env.DATABASE_USER || 'admin',
+  user: process.env.POSTGRES_USER || 'admin',
   host: process.env.DATABASE_HOST || 'localhost',
   database: process.env.DATABASE_DB || 'manufacturing_system',
-  password: process.env.DATABASE_PASSWORD || 'mypassword',
+  password: process.env.POSTGRES_PASSWORD || 'mypassword',
   port: Number(process.env.DATABASE_PORT) || 5432,
 };
+
 
 Object.freeze(config);
 
