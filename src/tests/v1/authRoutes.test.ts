dcdb2ce5fb22d94b008e0aff6e737ed1f2b105d5
Jose Szychowski <jszychowski.laba@solvd.com>
2023-11-02 20:08:15 -0300

    Create test

---
+++ src/tests/v1/authRoutes.test.ts
@@ -0,0 +1,66 @@
+import { startServer } from './helpers/startServer';
+import request from 'supertest';
+import app from '../../app';
+
+beforeAll(async () => {
+  await startServer();
+});
+
+const req = request(app);
+
+describe('Testing authRoutes', () => {
+  const authTokens = { access_token: null, refresh_token: null };
+  const refreshedAuthTokens = { access_token: null, refresh_token: null };
+
+  test('Expect POST: /api/v1/auth/login to return tokens', async () => {
+    const res = await req
+      .post('/api/v1/auth/login')
+      .set('Content-Type', 'application/x-www-form-urlencoded')
+      .send('client_id=admin&client_secret=admin')
+      .expect(201);
+
+    authTokens.access_token = res.body.access_token;
+    authTokens.refresh_token = res.body.refresh_token;
+  });
+
+  test('Expect POST: /api/v1/auth/refresh-tokens to refresh the tokens', async () => {
+    const res = await req
+      .post('/api/v1/auth/refresh-tokens')
+      .set('Content-Type', 'application/x-www-form-urlencoded')
+      .send(`refresh_token=${authTokens.refresh_token}`)
+      .expect(200);
+
+    refreshedAuthTokens.access_token = res.body.access_token;
+    refreshedAuthTokens.refresh_token = res.body.refresh_token;
+
+    expect(refreshedAuthTokens.access_token).not.toBe(authTokens.access_token);
+    expect(refreshedAuthTokens.refresh_token).not.toBe(
+      authTokens.refresh_token
+    );
+  });
+
+  test('Expect POST: /api/v1/auth/login to refresh the tokens', async () => {
+    const res = await req
+      .post('/api/v1/auth/login')
+      .set('Content-Type', 'application/x-www-form-urlencoded')
+      .send(`client_id=admin&client_secret=admin`)
+      .expect(201);
+
+    authTokens.access_token = res.body.access_token;
+    authTokens.refresh_token = res.body.refresh_token;
+
+    expect(refreshedAuthTokens.access_token).not.toBe(authTokens.access_token);
+    expect(refreshedAuthTokens.refresh_token).not.toBe(
+      authTokens.refresh_token
+    );
+  });
+
+  test('Expect DELETE: /api/v1/auth/logout to delete tokens', async () => {
+    const res = await req
+      .delete('/api/v1/auth/logout')
+      .set('authorization', `${authTokens.access_token}`)
+      .expect(204);
+
+    expect(res.body).toEqual({});
+  });
+});
