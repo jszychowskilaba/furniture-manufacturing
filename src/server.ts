898e4c72b25b1eb8055cd91cbc5a67905ca8c8c0
Jose Szychowski <jszychowski.laba@solvd.com>
2023-11-02 20:08:31 -0300

    Fix error of server starting with out the DBs

--- src/server.ts
+++ src/server.ts
@@ -5,25 +5,34 @@ import { initializeDataBase } from './databases/DataBase/initialization';
 // Starting redis client for Auth Data Base
 // AuthDB docker container must be already running
 (async () => {
+  let errorFlag = false;
+
   try {
     await initializeDataBase();
   } catch (error) {
     console.log(`Data Base error. ${error}`);
+    errorFlag = true;
   }
 
   TokensDB.on('error', (error: unknown) => {
     throw error;
   });
+
   try {
     await TokensDB.connect();
   } catch (error) {
     console.log('Redis TokensDB', error);
+    errorFlag = true;
   }
 
-  const PORT: number = Number(process.env.PORT) || 3000;
+  if (errorFlag) {
+    console.log('Something went wrong. Can not start app.');
+  } else {
+    // Staring server
+    const PORT: number = Number(process.env.PORT) || 3000;
 
-  // Staring server
-  app.listen(PORT, () => {
-    console.log(`API listening port ${PORT}...`);
-  });
+    app.listen(PORT, () => {
+      console.log(`API listening port ${PORT}...`);
+    });
+  }
 })();
