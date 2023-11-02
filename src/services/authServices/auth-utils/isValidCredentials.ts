a32c8cfd375e962baa66562b36a45f8cf25f25e9
Jose Szychowski <jszychowski.laba@solvd.com>
2023-11-02 10:47:27 -0300

    Add database support

--- src/services/authServices/auth-utils/isValidCredentials.ts
+++ src/services/authServices/auth-utils/isValidCredentials.ts
@@ -1,5 +1,7 @@
+import { Crypto } from '../../../helpers/Crypto';
+import { pool } from '../../../databases/DataBase/Pool';
 import { UserCredentials } from '../../../types/types';
-import * as Credentials from '../../../databases/Credentials';
+import { CustomError } from '../../../helpers/CustomError';
 
 /**
  * Checks if user credential es valid. Function will modified
@@ -7,9 +9,31 @@ import * as Credentials from '../../../databases/Credentials';
  * @param userCredentials The user credential
  * @returns true if valid, error if not
  */
-const isValidCredentials = (userCredentials: UserCredentials): boolean => {
-  const isValid = Credentials.isValidCredentials(userCredentials);
-  return isValid;
+const isValidCredentials = async (
+  userCredentials: UserCredentials
+): Promise<void> => {
+  try {
+    const { hashedPassword, salt } = (
+      await pool.query(
+        `SELECT "hashedPassword", "salt" FROM "appUser" WHERE "username" = '${userCredentials.client_id}'`
+      )
+    ).rows[0];
+
+    let isValid;
+    if (userCredentials.client_secret) {
+      isValid = new Crypto().isValidPassword(
+        userCredentials.client_secret,
+        hashedPassword,
+        salt
+      );
+    }
+
+    if (!isValid) {
+      throw new CustomError('Not valid client_ID or client_secret', 404);
+    }
+  } catch (error) {
+    throw new CustomError('Not valid client_ID or client_secret', 404);
+  }
 };
 
 export default isValidCredentials;
