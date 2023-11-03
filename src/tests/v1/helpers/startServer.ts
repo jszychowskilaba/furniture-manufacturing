136564199e3ed53f289e6f93d517439229320e7f
Jose Szychowski <jszychowski.laba@solvd.com>
2023-11-02 22:39:34 -0300

    Update to not load the app

--- src/tests/v1/helpers/startServer.ts
+++ src/tests/v1/helpers/startServer.ts
@@ -1,15 +1,11 @@
 import { initializeDataBase } from '../../../databases/DataBase/initialization';
 import { TokensDB } from '../../../databases/Auth/Auth';
-import app from '../../../app';
 
 export const startServer = async () => {
-  let errorFlag = false;
-
   try {
     await initializeDataBase();
   } catch (error) {
     console.log(`Data Base error. ${error}`);
-    errorFlag = true;
   }
 
   TokensDB.on('error', (error: unknown) => {
@@ -20,17 +16,5 @@ export const startServer = async () => {
     await TokensDB.connect();
   } catch (error) {
     console.log('Redis TokensDB', error);
-    errorFlag = true;
-  }
-
-  if (errorFlag) {
-    console.log('Something went wrong. Can not start app.');
-  } else {
-    // Staring server
-    const PORT: number = Number(process.env.PORT) || 3000;
-
-    app.listen(PORT, () => {
-      console.log(`API listening port ${PORT}...`);
-    });
   }
 };
