a05aa89117b77989f68922d3e2ad3e1550766437
Jose Szychowski <jszychowski.laba@solvd.com>
2023-11-02 21:28:53 -0300

    Update to parse Numeric to parseFloat

--- src/databases/DataBase/Pool.ts
+++ src/databases/DataBase/Pool.ts
@@ -1,5 +1,11 @@
 import { Pool } from 'pg';
 import { config } from '../../config/DataBase';
+import * as pg from 'pg';
+
+const types = pg.types;
+types.setTypeParser(1700, function (val) {
+  return parseFloat(val);
+});
 
 export const pool = new Pool({
   host: config.host,
