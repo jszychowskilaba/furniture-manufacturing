ce906d84e1cd1fa8b090280fd81fb584d0fa8dbd
Jose Szychowski <jszychowski.laba@solvd.com>
2023-11-02 21:28:26 -0300

    Create tests

---
+++ src/tests/v1/inventoryRoutes.test.ts
@@ -0,0 +1,124 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { startServer } from './helpers/startServer';
+import request from 'supertest';
+import app from '../../app';
+
+beforeAll(async () => {
+  await startServer();
+});
+
+const req = request(app);
+
+describe('Testing authRoutes', () => {
+  const authTokens = { access_token: null, refresh_token: null };
+  let createdMaterial1Id: any;
+
+  const material1: any = {
+    status: 'active',
+    internalCode: new Date().toISOString(), // to avoid unique column
+    description: 'Iron 3/8 nail for enforced wood',
+    stock: 1000,
+    reservedStock: 0,
+    pricePerUnit: 2,
+    unit: 'kg',
+    purchaseTime: 4,
+    internalNotes: 'used for making union of different parts',
+  };
+
+  test('Expect to login into the app', async () => {
+    const res = await req
+      .post('/api/v1/auth/login')
+      .set('Content-Type', 'application/x-www-form-urlencoded')
+      .send('client_id=admin&client_secret=admin')
+      .expect(201);
+
+    authTokens.access_token = res.body.access_token;
+    authTokens.refresh_token = res.body.refresh_token;
+  });
+
+  test('Expect POST: /api/v1/inventory to create a material', async () => {
+    const res = await req
+      .post('/api/v1/inventory')
+      .set('Content-Type', 'application/json')
+      .set('authorization', `${authTokens.access_token}`)
+      .send(material1)
+      .expect(201);
+
+    // Checking that all values are the same
+    Object.keys(material1).forEach((key) =>
+      expect(res.body[key]).toBe(material1[key])
+    );
+    createdMaterial1Id = res.body.id;
+  });
+
+  test('Expect GET: /api/v1/inventory/${materialId} to return the material', async () => {
+    const res = await req
+      .get(`/api/v1/inventory/${createdMaterial1Id}`)
+      .set('Content-Type', 'application/json')
+      .set('authorization', `${authTokens.access_token}`)
+      .expect(200);
+
+    // Checking that all values are the same
+    Object.keys(material1).forEach((key) =>
+      expect(res.body[key]).toBe(material1[key])
+    );
+  });
+
+  test('Expect GET: /api/v1/inventory/ to return all inventory', async () => {
+    // Adding extra material
+    const material2: any = {
+      status: 'active',
+      internalCode: new Date().toISOString(), // to avoid unique column
+      description: 'Iron 3/8 nail for enforced wood',
+      stock: 10,
+      reservedStock: 9,
+      pricePerUnit: 4,
+      unit: 'm',
+      purchaseTime: 1,
+      internalNotes: 'note',
+    };
+
+    await req
+      .post('/api/v1/inventory')
+      .set('Content-Type', 'application/json')
+      .set('authorization', `${authTokens.access_token}`)
+      .send(material2);
+
+    const res = await req
+      .get(`/api/v1/inventory/`)
+      .set('Content-Type', 'application/json')
+      .set('authorization', `${authTokens.access_token}`)
+      .expect(200);
+
+    for (const material of res.body) {
+      Object.keys(material1).forEach((key) =>
+        expect(material[key]).not.toBeNull()
+      );
+    }
+  });
+
+  test('Expect PATCH: /v1/inventory/{materialID} to update the material', async () => {
+    const patchMaterial: any = {
+      status: 'inactive',
+      internalCode: new Date().toISOString(), // to avoid unique column
+      description: 'newDescription',
+      stock: 0,
+      reservedStock: 0,
+      pricePerUnit: 0,
+      unit: 'r',
+      purchaseTime: 0,
+      internalNotes: 'newNote',
+    };
+
+    const res = await req
+      .patch(`/api/v1/inventory/${createdMaterial1Id}`)
+      .set('Content-Type', 'application/json')
+      .set('authorization', `${authTokens.access_token}`)
+      .send(patchMaterial)
+      .expect(200);
+
+    Object.keys(patchMaterial).forEach((key) =>
+      expect(res.body[key]).toBe(patchMaterial[key])
+    );
+  });
+});
---
+++ src/tests/v1/laborRoutes.test.ts
@@ -0,0 +1,116 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { startServer } from './helpers/startServer';
+import request from 'supertest';
+import app from '../../app';
+
+beforeAll(async () => {
+  await startServer();
+});
+
+const req = request(app);
+
+describe('Testing authRoutes', () => {
+  const authTokens = { access_token: null, refresh_token: null };
+  let createdLabor1Id: any;
+
+  const labor1: any = {
+    status: 'active',
+    internalCode: new Date().toISOString(),
+    description: 'Cutting board for hardwood table top',
+    pricePerUnit: 1,
+    timePerUnit: 300,
+    unit: 'm',
+    internalNotes: 'Maximum saw speed: 3',
+  };
+
+  test('Expect to login into the app', async () => {
+    const res = await req
+      .post('/api/v1/auth/login')
+      .set('Content-Type', 'application/x-www-form-urlencoded')
+      .send('client_id=admin&client_secret=admin')
+      .expect(201);
+
+    authTokens.access_token = res.body.access_token;
+    authTokens.refresh_token = res.body.refresh_token;
+  });
+
+  test('Expect POST: /api/v1/labor to create a labor', async () => {
+    const res = await req
+      .post('/api/v1/labor')
+      .set('Content-Type', 'application/json')
+      .set('authorization', `${authTokens.access_token}`)
+      .send(labor1)
+      .expect(201);
+
+    // Checking that all values are the same
+    Object.keys(labor1).forEach((key) =>
+      expect(res.body[key]).toBe(labor1[key])
+    );
+    createdLabor1Id = res.body.id;
+  });
+
+  test('Expect GET: /api/v1/labor/${laborId} to return the labor', async () => {
+    const res = await req
+      .get(`/api/v1/labor/${createdLabor1Id}`)
+      .set('Content-Type', 'application/json')
+      .set('authorization', `${authTokens.access_token}`)
+      .expect(200);
+
+    // Checking that all values are the same
+    Object.keys(labor1).forEach((key) =>
+      expect(res.body[key]).toBe(labor1[key])
+    );
+  });
+
+  test('Expect GET: /api/v1/labor/ to return all labors', async () => {
+    // Adding extra labor
+    const labor2: any = {
+      status: 'inactive',
+      internalCode: new Date().toISOString(),
+      description: 'Cutting foam',
+      pricePerUnit: 5,
+      timePerUnit: 2,
+      unit: 'm',
+      internalNotes: 'Maximum saw speed: 3',
+    };
+
+    await req
+      .post('/api/v1/labor')
+      .set('Content-Type', 'application/json')
+      .set('authorization', `${authTokens.access_token}`)
+      .send(labor2);
+
+    const res = await req
+      .get(`/api/v1/labor/`)
+      .set('Content-Type', 'application/json')
+      .set('authorization', `${authTokens.access_token}`)
+      .expect(200);
+
+    for (const labor of res.body) {
+      Object.keys(labor1).forEach((key) => expect(labor[key]).not.toBeNull());
+    }
+  });
+
+  test('Expect PATCH: /v1/labor/{laborID} to update the labor', async () => {
+    const patchLabor: any = {
+      status: 'inactive',
+      internalCode: new Date().toISOString(),
+      description: 'updatedDescription',
+      pricePerUnit: 0,
+      timePerUnit: 0,
+      unit: 'kg',
+      internalNotes: 'Maximum saw speed: 4',
+    };
+
+    const res = await req
+      .patch(`/api/v1/labor/${createdLabor1Id}`)
+      .set('Content-Type', 'application/json')
+      .set('authorization', `${authTokens.access_token}`)
+      .send(patchLabor)
+      .expect(200);
+
+    Object.keys(patchLabor).forEach((key) =>
+      expect(res.body[key]).toBe(patchLabor[key])
+    );
+  });
+});
