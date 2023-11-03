55cc127a4423628238b36fca48495ac93b053efd
Jose Szychowski <jszychowski.laba@solvd.com>
2023-11-03 10:04:00 -0300

    Create test

---
+++ src/tests/v1/orderRoutes.test.ts
@@ -0,0 +1,210 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { startServer } from './helpers/startServer';
+import { generateRandomId } from './helpers/generateRandomId';
+import request from 'supertest';
+import app from '../../app';
+
+beforeAll(async () => {
+  console.log = jest.fn();
+  await startServer();
+});
+
+const req = request(app);
+
+describe('Testing orderRoutes', () => {
+  const authTokens = { access_token: null, refresh_token: null };
+
+  let manufactureOrder: any = {};
+  let createdManufactureOrder: any = {};
+  let createdMaterial1: any = {};
+  let createdMaterial2: any = {};
+  let createdLabor1: any = {};
+  let createdLabor2: any = {};
+
+  const material1 = {
+    status: 'active',
+    internalCode: generateRandomId(), // to avoid unique column
+    description: 'Iron 3/8 nail for enforced wood',
+    stock: 1000,
+    reservedStock: 0,
+    pricePerUnit: 2,
+    unit: 'kg',
+    purchaseTime: 4,
+    internalNotes: 'used for making union of different parts',
+  };
+
+  const material2 = {
+    status: 'active',
+    internalCode: generateRandomId(), // to avoid unique column
+    description: 'Iron 3/8 nail for enforced wood',
+    stock: 500,
+    reservedStock: 0,
+    pricePerUnit: 1,
+    unit: 'm',
+    purchaseTime: 10,
+    internalNotes: 'used for making union of different parts',
+  };
+
+  const labor1 = {
+    status: 'active',
+    internalCode: generateRandomId(),
+    description: 'Cutting board for hardwood table top',
+    pricePerUnit: 3,
+    timePerUnit: 300,
+    unit: 'm',
+    internalNotes: 'Maximum saw speed: 3',
+  };
+
+  const labor2 = {
+    status: 'active',
+    internalCode: generateRandomId(),
+    description: 'Hardwood joint',
+    pricePerUnit: 5,
+    timePerUnit: 300,
+    unit: 'm',
+    internalNotes: 'Maximum saw speed: 3',
+  };
+
+  test('Expect to login into the app', async () => {
+    const res = await req
+      .post('/api/v1/auth/login')
+      .set('Content-Type', 'application/x-www-form-urlencoded')
+      .send('client_id=admin&client_secret=admin')
+      .expect(201);
+
+    authTokens.access_token = res.body.access_token;
+    authTokens.refresh_token = res.body.refresh_token;
+  });
+
+  test('Expect materials and labors to be created', async () => {
+    const mat1 = await req
+      .post('/api/v1/inventory')
+      .set('Content-Type', 'application/json')
+      .set('authorization', `${authTokens.access_token}`)
+      .send(material1)
+      .expect(201);
+
+    createdMaterial1 = { ...mat1.body };
+
+    const mat2 = await req
+      .post('/api/v1/inventory')
+      .set('Content-Type', 'application/json')
+      .set('authorization', `${authTokens.access_token}`)
+      .send(material2)
+      .expect(201);
+
+    createdMaterial2 = { ...mat2.body };
+
+    const lab1 = await req
+      .post('/api/v1/labor')
+      .set('Content-Type', 'application/json')
+      .set('authorization', `${authTokens.access_token}`)
+      .send(labor1)
+      .expect(201);
+
+    createdLabor1 = { ...lab1.body };
+
+    const lab2 = await req
+      .post('/api/v1/labor')
+      .set('Content-Type', 'application/json')
+      .set('authorization', `${authTokens.access_token}`)
+      .send(labor2)
+      .expect(201);
+
+    createdLabor2 = { ...lab2.body };
+  });
+
+  test('Expect POST: /api/v1/order to create a manufacture order', async () => {
+    manufactureOrder = {
+      status: 'inProduction',
+      internalCode: generateRandomId(),
+      description: 'Basic wood table for client Bob Doe',
+      unitsToManufacture: 3,
+      internalNotes: 'See design sent to email @July 23',
+      materials: [
+        { id: createdMaterial1.id, quantity: 20 },
+        { id: createdMaterial2.id, quantity: 10 },
+      ],
+      labors: [
+        { id: createdLabor1.id, quantity: 10 },
+        { id: createdLabor2.id, quantity: 20 },
+      ],
+    };
+
+    const res = await req
+      .post('/api/v1/orders')
+      .set('Content-Type', 'application/json')
+      .set('authorization', `${authTokens.access_token}`)
+      .send(manufactureOrder)
+      .expect(201);
+
+    createdManufactureOrder = res.body;
+
+    manufactureOrder.totalPrice =
+      material1.pricePerUnit * 20 * 3 +
+      material2.pricePerUnit * 10 * 3 +
+      labor1.pricePerUnit * 10 * 3 +
+      labor2.pricePerUnit * 20 * 3;
+
+    manufactureOrder.totalProductionTime =
+      labor1.timePerUnit * 10 * 3 + labor2.timePerUnit * 20 * 3;
+
+    delete manufactureOrder.materials;
+    delete manufactureOrder.labors;
+
+    Object.keys(manufactureOrder).forEach((key) =>
+      expect(createdManufactureOrder[key]).toBe(manufactureOrder[key])
+    );
+  });
+
+  test('Expect materials stock and reserved stock to be updated after a order', async () => {
+    const res1 = await req
+      .get(`/api/v1/inventory/${createdMaterial1.id}`)
+      .set('Content-Type', 'application/json')
+      .set('authorization', `${authTokens.access_token}`)
+      .expect(200);
+
+    expect(res1.body.stock).toBe(material1.stock - 20 * 3);
+    expect(res1.body.reservedStock).toBe(material1.reservedStock + 20 * 3);
+
+    const res2 = await req
+      .get(`/api/v1/inventory/${createdMaterial2.id}`)
+      .set('Content-Type', 'application/json')
+      .set('authorization', `${authTokens.access_token}`)
+      .expect(200);
+
+    expect(res2.body.stock).toBe(material2.stock - 10 * 3);
+    expect(res2.body.reservedStock).toBe(material2.reservedStock + 10 * 3);
+  });
+
+  test('Expect POST: /v1/orders/{orderID}/manufactureOrder to manufacture and update materials', async () => {
+    const toManufacture = {
+      quantity: 2,
+    };
+
+    await req
+      .post(`/api/v1/orders/${createdManufactureOrder.id}/manufactureOrder`)
+      .set('Content-Type', 'application/json')
+      .set('authorization', `${authTokens.access_token}`)
+      .send(toManufacture)
+      .expect(200);
+
+    const res1 = await req
+      .get(`/api/v1/inventory/${createdMaterial1.id}`)
+      .set('Content-Type', 'application/json')
+      .set('authorization', `${authTokens.access_token}`)
+      .expect(200);
+
+    expect(res1.body.stock).toBe(material1.stock - 20 * 3);
+    expect(res1.body.reservedStock).toBe(20 * 1);
+
+    const res2 = await req
+      .get(`/api/v1/inventory/${createdMaterial2.id}`)
+      .set('Content-Type', 'application/json')
+      .set('authorization', `${authTokens.access_token}`)
+      .expect(200);
+
+    expect(res2.body.stock).toBe(material2.stock - 10 * 3);
+    expect(res2.body.reservedStock).toBe(10 * 1);
+  });
+});
