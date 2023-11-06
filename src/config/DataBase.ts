ea538c6514c53408fe535d616ca9e96db448966d
Jose Szychowski <jszychowski.laba@solvd.com>
2023-11-06 16:22:48 -0300

    Update default password

--- src/config/DataBase.ts
+++ src/config/DataBase.ts
@@ -2,7 +2,7 @@ const config = {
   user: process.env.POSTGRES_USER || 'admin',
   host: process.env.DATABASE_HOST || 'localhost',
   database: process.env.DATABASE_DB || 'manufacturing_system',
-  password: process.env.POSTGRES_PASSWORD || 'mypassword',
+  password: process.env.POSTGRES_PASSWORD || 'admin',
   port: Number(process.env.DATABASE_PORT) || 5432,
 };
 
