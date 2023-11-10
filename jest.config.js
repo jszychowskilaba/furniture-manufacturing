e0ac2c3593942a1a65123a1e8f0002f3da72411b
Jose Szychowski <jszychowski.laba@solvd.com>
2023-11-09 21:03:02 -0300

    Revert "Update file to ts"
    This reverts commit d2774fe33fc57554656ab15867c77124b82b6fac.

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,14 @@
-import type { Config } from 'jest';
-
-const config: Config = {
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  collectCoverage: true,
   coverageThreshold: {
     global: {
       branches: 65,
       functions: 65,
       lines: 65,
       statements: 65,
+      collectCoverage: true,
     },
   },
 };
-
-export default config;
