2ee984a53b00b99866bfec189134cc342693630b
Jose Szychowski <jszychowski.laba@solvd.com>
2023-11-09 21:04:14 -0300

    Fix error

--- jest.config.js
+++ jest.config.js
@@ -2,13 +2,13 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  collectCoverage: true,
   coverageThreshold: {
     global: {
       branches: 65,
       functions: 65,
       lines: 65,
       statements: 65,
-      collectCoverage: true,
     },
   },
 };
